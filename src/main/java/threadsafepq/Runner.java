53d40c9d6e201b0b04a75f8a7c28bbaf9ca50657
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-05-26 13:46:28 +1200

    Added main method

--- src/main/java/threadsafepq/Runner.java
+++ src/main/java/threadsafepq/Runner.java
@@ -4,4 +4,8 @@ package threadsafepq;
  * Created by Taranpreet on 26/05/2017.
  */
 public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
