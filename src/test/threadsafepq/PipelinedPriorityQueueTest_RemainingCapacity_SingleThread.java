644c37fcdfd7528aeb57f33b981c8906f7204637
taranpreetkohli <tkoh638@aucklanduni.ac.nz>
2017-06-04 20:45:47 +1200

    Implement unit tests for remainingSize()

---
+++ src/test/threadsafepq/PipelinedPriorityQueueTest_RemainingCapacity_SingleThread.java
@@ -0,0 +1,48 @@
+package threadsafepq;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PipelinedPriorityQueueTest_RemainingCapacity_SingleThread extends PipelinedPriorityQueueTest {
+
+    @Test
+    public void RemainingCapacity_DefaultQueueEmpty_ReturnsMaxInteger() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
+        assertEquals(Integer.MAX_VALUE, queue.remainingCapacity());
+    }
+
+    @Test
+    public void RemainingCapacity_DefaultQueueNonEmpty_ReturnsMaxInteger() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
+        queue.put(1);
+        assertEquals(Integer.MAX_VALUE, queue.remainingCapacity());
+    }
+
+    @Test
+    public void RemainingCapacity_CapacityQueueEmpty_ReturnsMaxInteger() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        assertEquals(Integer.MAX_VALUE, queue.remainingCapacity());
+    }
+
+    @Test
+    public void RemainingCapacity_CapacityQueueNonEmpty_ReturnsMaxInteger() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        queue.put(1);
+        assertEquals(Integer.MAX_VALUE, queue.remainingCapacity());
+    }
+
+    @Test
+    public void RemainingCapacity_CapacityComparatorQueueEmpty_ReturnsMaxInteger() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        assertEquals(Integer.MAX_VALUE, queue.remainingCapacity());
+    }
+
+    @Test
+    public void RemainingCapacity_CapacityComparatorQueueNonEmpty_ReturnsMaxInteger() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        queue.put(1);
+        assertEquals(Integer.MAX_VALUE, queue.remainingCapacity());
+    }
+
+}
