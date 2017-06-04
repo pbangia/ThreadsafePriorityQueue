efc8bebe004d7a9496638e74e24d5dbe9dacfc71
taranpreetkohli <tkoh638@aucklanduni.ac.nz>
2017-06-04 17:15:55 +1200

    Add tests for contains()

--- src/test/threadsafepq/PipelinedPriorityQueueTest_Contains_SingleThread.java
+++ src/test/threadsafepq/PipelinedPriorityQueueTest_Contains_SingleThread.java
@@ -3,54 +3,158 @@ package threadsafepq;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import static junit.framework.TestCase.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Taranpreet on 3/06/2017.
  */
 public class PipelinedPriorityQueueTest_Contains_SingleThread {
 
-    private PipelinedPriorityQueue<Integer> queue;
+    private static final int DEFAULT_INITIAL_CAPACITY = 11;
+    private static final int CUSTOM_INITIAL_CAPACITY = 42;
+    private PipelinedPriorityQueue<Integer> defaultQueue;
+    private PipelinedPriorityQueue<Integer> capacityQueue;
+    private PipelinedPriorityQueue<Integer> capacityComparatorQueue;
 
     @Before
     public void before() {
-        queue = new PipelinedPriorityQueue<Integer>();
+        defaultQueue = new PipelinedPriorityQueue<>();
+        capacityQueue = new PipelinedPriorityQueue<>(CUSTOM_INITIAL_CAPACITY);
+        capacityComparatorQueue = new PipelinedPriorityQueue<>(CUSTOM_INITIAL_CAPACITY, (o1, o2) -> {
+            int result = o1.compareTo(o2);
+            if (result == -1) return 1;
+            if (result == 1) return -1;
+            return result;
+        });
     }
 
     @Test
-    public void Put_DefaultQueueMultipleItems3_CorrectContains() {
-        for (int i = 0; i < 6; i++) {
-            assertFalse(queue.contains(i));
-        }
+    public void Contains_DefaultQueueEmpty_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
+        assertEquals(false, queue.contains(0));
+    }
 
+    @Test
+    public void Contains_DefaultQueueSingleItemWhichExists_ReturnsTrue() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
         queue.put(0);
-        queue.put(2);
-        queue.put(5);
+        assertEquals(true, queue.contains(0));
+    }
+
+    @Test
+    public void Contains_DefaultQueueSingleItemWhichDoesNotExist_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
+        queue.put(0);
+        assertEquals(false, queue.contains(1));
+    }
+
+    @Test
+    public void Contains_DefaultQueueMultipleItemsWhichExist_ReturnsTrue() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
+        queue.put(0);
         queue.put(1);
         queue.put(3);
-        queue.put(4);
-
-        for (int i = 0; i < 6; i++) {
-            assertTrue(queue.contains(i));
-        }
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(4);
-        list.add(3);
-        list.add(2);
-        list.add(0);
-        list.add(1);
-        list.add(5);
-        assertTrue(queue.containsAll(list));
-        list.remove(0);
-        list.remove(1);
-        assertTrue(queue.containsAll(list));
-
-        list.add(8);
-        assertFalse(queue.containsAll(list));
+        assertEquals(true, queue.contains(0));
+        assertEquals(true, queue.contains(1));
+        assertEquals(true, queue.contains(3));
     }
+
+    @Test
+    public void Contains_DefaultQueueMultipleItemsWhichDoNotExist_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = defaultQueue;
+        queue.put(0);
+        queue.put(1);
+        queue.put(3);
+        assertEquals(false, queue.contains(4));
+        assertEquals(false, queue.contains(5));
+    }
+
+    // break
+
+    @Test
+    public void Contains_CapacityQueueEmpty_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        assertEquals(false, queue.contains(0));
+    }
+
+    @Test
+    public void Contains_CapacityQueueSingleItemWhichExists_ReturnsTrue() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        queue.put(0);
+        assertEquals(true, queue.contains(0));
+    }
+
+    @Test
+    public void Contains_CapacityQueueSingleItemWhichDoesNotExist_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        queue.put(0);
+        assertEquals(false, queue.contains(1));
+    }
+
+    @Test
+    public void Contains_CapacityQueueMultipleItemsWhichExist_ReturnsTrue() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        queue.put(0);
+        queue.put(1);
+        queue.put(3);
+        assertEquals(true, queue.contains(0));
+        assertEquals(true, queue.contains(1));
+        assertEquals(true, queue.contains(3));
+    }
+
+    @Test
+    public void Contains_CapacityQueueMultipleItemsWhichDoNotExist_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = capacityQueue;
+        queue.put(0);
+        queue.put(1);
+        queue.put(3);
+        assertEquals(false, queue.contains(4));
+        assertEquals(false, queue.contains(5));
+    }
+
+    // break 
+
+    @Test
+    public void Contains_CapacityComparatorQueueEmpty_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        assertEquals(false, queue.contains(0));
+    }
+
+    @Test
+    public void Contains_CapacityComparatorQueueSingleItemWhichExists_ReturnsTrue() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        queue.put(0);
+        assertEquals(true, queue.contains(0));
+    }
+
+    @Test
+    public void Contains_CapacityComparatorQueueSingleItemWhichDoesNotExist_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        queue.put(0);
+        assertEquals(false, queue.contains(1));
+    }
+
+    @Test
+    public void Contains_CapacityComparatorQueueMultipleItemsWhichExist_ReturnsTrue() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        queue.put(0);
+        queue.put(1);
+        queue.put(3);
+        assertEquals(true, queue.contains(0));
+        assertEquals(true, queue.contains(1));
+        assertEquals(true, queue.contains(3));
+    }
+
+    @Test
+    public void Contains_CapacityComparatorQueueMultipleItemsWhichDoNotExist_ReturnsFalse() {
+        PipelinedPriorityQueue<Integer> queue = capacityComparatorQueue;
+        queue.put(0);
+        queue.put(1);
+        queue.put(3);
+        assertEquals(false, queue.contains(4));
+        assertEquals(false, queue.contains(5));
+    }
+
+
 
 }
