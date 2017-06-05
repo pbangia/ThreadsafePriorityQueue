8e97f9a5b18637a2c1b6892d33d87da77508101a
g.tiongco <g.tiongco95@gmail.com>
2017-06-05 17:11:32 +1200

    Refactored BenchmarkTest

--- src/test/threadsafepq/BenchmarkTest.java
+++ src/test/threadsafepq/BenchmarkTest.java
@@ -1,82 +1,55 @@
 package threadsafepq;
 
-import jdk.internal.util.xml.impl.Input;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
-
-import static junit.framework.TestCase.assertFalse;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class BenchmarkTest {
 
-    private PipelinedPriorityQueue<Integer> defaultQueue;
-    private PipelinedPriorityQueue<Integer> pipelinedQ;
+    private PipelinedPriorityQueue<Integer> pipelinedQueue;
     private PriorityBlockingQueue<Integer> blockingQ;
+    private enum OperationType {PUT_RANDOM,PUT_ORDERED,PUT_REVERSED,MIXED,POLL}
+    private enum QueueType {BLOCKING, PIPELINED_DEFAULT, PIPELINED_CAPACITY}
+    private int[] inputSizes = new int[]{};
 
     static final int NUMBER_OF_TESTS = 10;
 
     @Before
     public void before() {
-        defaultQueue = new PipelinedPriorityQueue<Integer>();
+        pipelinedQueue = new PipelinedPriorityQueue<Integer>();
         blockingQ = new PriorityBlockingQueue<Integer>();
-        pipelinedQ = new PipelinedPriorityQueue<Integer>(1000001);
         PriorityBlockingQueue<Integer> q = new PriorityBlockingQueue<Integer>(100001);
         for (int i=0; i<100000; i++) q.put(i);
 
     }
 
     @Test
-    public void Put_BlockingQueueTiming(){
-        long total=0;
-        for (int j=0; j<10; j++) {
-            long start = System.currentTimeMillis();
-            for (int i = 0; i < 1000000; i++) {
-                blockingQ.put(i);
-            }
-            long end = System.currentTimeMillis();
-            blockingQ.clear();
-            long elapsed = (end-start);
-            total+=elapsed;
-        }
-
-        System.out.println("PriorityBlockingQueue time:" + total/10);
-    }
-
-    @Test
-    public void Put_PipelinedTiming() {
-        long total=0;
-        for (int j=0; j<1; j++) {
-            long start = System.currentTimeMillis();
-            for (int i = 0; i < 10000; i++) {
-                pipelinedQ.put(i);
-            }
-            long end = System.currentTimeMillis();
-            pipelinedQ.clear();
-            long elapsed = (end-start);
-            total+=elapsed;
-        }
-        System.out.println("PipelinedPriorityQueue time:" + total/10);
-
-    }
-
-    @Test
-    public void Put_InputSize10000DefaultQueueInOrder_TimedExecution() {
+    public void Put_InputSize10000DefaultPipelinedQueuePutOrdered_TimedExecution() throws InterruptedException{
         int inputSize = 10000;
-        boolean isDefaultQueue = true;
-        InputType type = InputType.IN_ORDER;
+        QueueType queueType = QueueType.PIPELINED_DEFAULT;
+        OperationType operationType = OperationType.PUT_ORDERED;
 
-        sequential_Test(inputSize, isDefaultQueue, type);
+        runSequentialTest(inputSize, queueType, operationType);
     }
 
-    private void sequential_Test(int inputSize, boolean isDefaultQueue, InputType type) {
+    private void runSequentialTest(int inputSize, QueueType queueType, OperationType operationType) throws InterruptedException{
+        BlockingQueue<Integer> queue;
         long total = 0;
-        PipelinedPriorityQueue<Integer> queue = (isDefaultQueue) ? defaultQueue : new PipelinedPriorityQueue<>(inputSize);
-        int[] inputList = generateInputListFromType(inputSize, type);
+        int[] inputList = generateInputListFromType(inputSize, operationType);
+
+        switch(queueType) {
+            case BLOCKING:
+                queue = blockingQ;
+                break;
+            case PIPELINED_DEFAULT:
+                queue = pipelinedQueue;
+                break;
+            default:
+                queue = new PipelinedPriorityQueue<>(inputSize);
+                break;
+        }
 
         for (int j = 0; j < NUMBER_OF_TESTS; j++) {
             long start = System.currentTimeMillis();
@@ -84,23 +57,24 @@ public class BenchmarkTest {
                 queue.put(inputList[i]);
             }
             long end = System.currentTimeMillis();
-            defaultQueue.clear();
+            queue.clear();
             total += (end - start);
         }
         System.out.println("Input Size: " + inputSize +
-                ", Default Queue: " + isDefaultQueue +
-                ", Input Type: " + type +
+                ", Queue Type: " + queueType +
+                ", Operation Time: " + operationType +
                 ", Execution time: " + total/NUMBER_OF_TESTS);
+
     }
 
-    private int[] generateInputListFromType(int inputSize, InputType type) {
+    private int[] generateInputListFromType(int inputSize, OperationType type) {
         int[] list = new int[inputSize];
 
         for (int i = 0; i < list.length; i++) {
-            list[i] = (type == InputType.IN_ORDER) ? i : list.length - i;
+            list[i] = (type == OperationType.PUT_ORDERED) ? i : list.length - i;
         }
 
-        if (type == InputType.SHUFFLE) shuffleArray(list);
+        if (type == OperationType.PUT_RANDOM) shuffleArray(list);
 
         return list;
     }
@@ -116,12 +90,6 @@ public class BenchmarkTest {
                 array[index] ^= array[i];
             }
         }
-    }
-
-    protected enum InputType {
-        IN_ORDER,
-        REVERSE_ORDER,
-        SHUFFLE
     }
 
 }
