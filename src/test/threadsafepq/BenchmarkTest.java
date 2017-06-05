9e0aa594246bc726541d150ef096e034c9a27fbb
g.tiongco <g.tiongco95@gmail.com>
2017-06-05 12:39:41 +1200

    Add generic method for testing sequential execution time
    Added Put_InputSize10000DefaultQueueInOrder_TimedExecution test and sequentialTest, generateInputListFromType, shuffleArray methods

--- src/test/threadsafepq/BenchmarkTest.java
+++ src/test/threadsafepq/BenchmarkTest.java
@@ -1,5 +1,6 @@
 package threadsafepq;
 
+import jdk.internal.util.xml.impl.Input;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,11 +14,15 @@ import static org.junit.Assert.assertTrue;
 
 public class BenchmarkTest {
 
+    private PipelinedPriorityQueue<Integer> defaultQueue;
     private PipelinedPriorityQueue<Integer> pipelinedQ;
     private PriorityBlockingQueue<Integer> blockingQ;
 
+    static final int NUMBER_OF_TESTS = 10;
+
     @Before
     public void before() {
+        defaultQueue = new PipelinedPriorityQueue<Integer>();
         blockingQ = new PriorityBlockingQueue<Integer>();
         pipelinedQ = new PipelinedPriorityQueue<Integer>(1000001);
         PriorityBlockingQueue<Integer> q = new PriorityBlockingQueue<Integer>(100001);
@@ -59,6 +64,64 @@ public class BenchmarkTest {
 
     }
 
+    @Test
+    public void Put_InputSize10000DefaultQueueInOrder_TimedExecution() {
+        int inputSize = 10000;
+        boolean isDefaultQueue = true;
+        InputType type = InputType.IN_ORDER;
 
+        sequential_Test(inputSize, isDefaultQueue, type);
+    }
+
+    private void sequential_Test(int inputSize, boolean isDefaultQueue, InputType type) {
+        long total = 0;
+        PipelinedPriorityQueue<Integer> queue = (isDefaultQueue) ? defaultQueue : new PipelinedPriorityQueue<>(inputSize);
+        int[] inputList = generateInputListFromType(inputSize, type);
+
+        for (int j = 0; j < NUMBER_OF_TESTS; j++) {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < inputSize; i++) {
+                queue.put(inputList[i]);
+            }
+            long end = System.currentTimeMillis();
+            defaultQueue.clear();
+            total += (end - start);
+        }
+        System.out.println("Input Size: " + inputSize +
+                ", Default Queue: " + isDefaultQueue +
+                ", Input Type: " + type +
+                ", Execution time: " + total/NUMBER_OF_TESTS);
+    }
+
+    private int[] generateInputListFromType(int inputSize, InputType type) {
+        int[] list = new int[inputSize];
+
+        for (int i = 0; i < list.length; i++) {
+            list[i] = (type == InputType.IN_ORDER) ? i : list.length - i;
+        }
+
+        if (type == InputType.SHUFFLE) shuffleArray(list);
+
+        return list;
+    }
+
+    private void shuffleArray(int[] array) {
+        int index;
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            index = random.nextInt(i + 1);
+            if (index != i) {
+                array[index] ^= array[i];
+                array[i] ^= array[index];
+                array[index] ^= array[i];
+            }
+        }
+    }
+
+    protected enum InputType {
+        IN_ORDER,
+        REVERSE_ORDER,
+        SHUFFLE
+    }
 
 }
