554b58a6ec3c1c1d10c032ecf564c43ea98d2313
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2017-06-07 10:54:25 +1200

    Insert JavaDocs for BinaryArrayElement

--- src/main/java/threadsafepq/BinaryArrayElement.java
+++ src/main/java/threadsafepq/BinaryArrayElement.java
@@ -7,17 +7,42 @@ import java.io.Serializable;
 import java.util.Comparator;
 
 /**
- * Created by Taranpreet on 26/05/2017.
+ * A single instance of BinaryArrayElement<E> represents
+ * a single node in the heap (binary tree) of the pipelined heap used for the queue.
  */
 public class BinaryArrayElement<E> implements Serializable {
 
+    /**
+     * Used to uniquely serialize an instance of this class.
+     */
     private static final long serialVersionUID = 43L;
 
+    /**
+     * True if this node is a non-null entry in the heap, false otherwise.
+     */
     private boolean isActive;
+    /**
+     * Represents the generic type value to be stored inside the node.
+     */
     private E value;
+    /**
+     * Represents the number of inactive nodes sub-rooted at this node.
+     */
     private int capacity;
+    /**
+     * Represents the user-defined comparator passed in the construction of the queue.
+     * This is used to determine the relative priorities of different nodes.
+     */
     private Comparator<? super E> comparator;
 
+    /**
+     * Constructs a single instance of BinaryArrayElement<E>
+     *
+     * @param isActive   true if this node is active on construction
+     * @param value      nullable value representing the node
+     * @param capacity   number of inactive nodes sub-rooted at this node
+     * @param comparator comparator used to determine the relative prorities of different nodes
+     */
     public BinaryArrayElement(boolean isActive, E value, int capacity, Comparator<? super E> comparator) {
         this.isActive = isActive;
         this.value = value;
@@ -25,10 +50,21 @@ public class BinaryArrayElement<E> implements Serializable {
         this.comparator = comparator;
     }
 
+    /**
+     * Returns true if this BinaryArrayElement is active
+     * @return true if this BinaryArrayElement is active
+     */
     public boolean isActive() {
         return isActive;
     }
 
+    /**
+     * Returns true if the value in this BinaryArrayElement is greater than the argument value.
+     * This implementation will use natural ordering to determine the relativity in priorities between
+     * the two values if no comparator is provided.
+     * @param o The other value to compare this BinaryArrayElement's value with
+     * @return true if the value in this BinaryArrayElement is greater than the argument value
+     */
     public boolean isGreaterThan(E o) {
         int result;
         if (comparator != null) {
@@ -40,36 +76,58 @@ public class BinaryArrayElement<E> implements Serializable {
         return (result > 0);
     }
 
+    /**
+     * Decrements the capacity of this BinaryArrayElement.
+     */
     public void decrementCapacity() {
         this.capacity--;
     }
 
+    /**
+     * Increments the capacity of this BinaryArrayElement.
+     */
     public void incrementCapacity() {
         this.capacity++;
     }
 
+    /**
+     * Sets the active field to the argument provided.
+     * @param active true if this instance's active field needs to be set to true, false otherwise.
+     */
     public void setActive(boolean active) {
         isActive = active;
     }
 
+    /**
+     * Returns the value associated with this BinaryArrayElement
+     * @return the value associated with this BinaryArrayElement
+     */
     public E getValue() {
         return value;
     }
 
+    /**
+     * Sets the value associated with this BinaryArrayElement
+     * @param value the new value to be associated with this BinaryArrayElement
+     */
     public void setValue(E value) {
         this.value = value;
     }
 
+    /**
+     * Returns the capacity of this BinaryArrayElement
+     * @return the capacity of this BinaryArrayElement
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Sets the capacity of this BinaryArrayElement
+     * @param capacity the new capacity of this BinaryArrayElement
+     */
     public void setCapacity(int capacity) {
         this.capacity = capacity;
-    }
-
-    public Comparator<? super E> getComparator() {
-        return comparator;
     }
 
     @Override
--- src/main/java/threadsafepq/Runner.java
+++ src/main/java/threadsafepq/Runner.java
@@ -1,20 +1,14 @@
 package threadsafepq;
 
-import java.util.Arrays;
-
 /**
  * Created by Taranpreet on 26/05/2017.
  */
 public class Runner {
 
     public static void main(String[] args) {
-        PipelinedPriorityQueue<Integer> p = new PipelinedPriorityQueue<Integer>();
+        PipelinedPriorityQueue<Integer> queue = new PipelinedPriorityQueue<>();
+        System.out.println("Hello world");
 
-        int[] array = {1, 3, 5, 6, 7};
-        int[] oldArray = array;
-        array = new int[5];
-
-        System.out.println(Arrays.toString(oldArray));
-        System.out.println(Arrays.toString(array));
+        // Feel free to use the Pipelined Priority Queue in this class for testing!
     }
 }
